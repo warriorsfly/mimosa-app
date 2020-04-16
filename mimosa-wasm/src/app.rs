b241afc0208d225bb66c9086cbc0c2ba862b27cc
walker <warriorsfly@gmail.com>
2020-04-16 13:18:02 +0800

    init

---
+++ mimosa-wasm/src/app.rs
@@ -0,0 +1,3 @@
+use yew::services::fetch::FetchTask;
+use yew::{agent::Bridged, html, Bridge, Callback, Component, ComponentLink, Html, ShouldRender};
+use yew_router::prelude::*;
--- mimosa-wasm/src/components/header.rs
+++ mimosa-wasm/src/components/header.rs
@@ -18,5 +18,59 @@ impl Compenent for Header {
     type Message = Msg;
     type Properties = Props;
 
-    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {}
+    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Header { props }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.props = props;
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <nav class="navbar navbar-light">
+                <div class="container">
+                    <RouterAnchor<AppRoute> route=AppRoute::Home class="navbar-brand">
+                        {"Mimosa"}
+                    </RouterAnchor<AppRoute>>
+                    {
+                        if let Some(account) = &self.props.current{
+                            self.logged_in_view(&account)
+                        }else{
+                            self.logged_out_view()
+                        }
+                    }
+                </div>
+            </nav>
+        }
+    }
+}
+
+impl Header {
+    fn logged_out_view(&self) -> Html {
+        html! {
+            <ul class="nav navbar-nav pull-xs-right">
+                <li class="nav-item">
+                    <RouterAnchor<AppRoute> route=AppRoute::Home classes="nav-link">
+                        { "Home" }
+                    </RouterAnchor<AppRoute>>
+                </li>
+                <li class="nav-item">
+                    <RouterAnchor<AppRoute> route=AppRoute::Login classes="nav-link">
+                        { "Sign in" }
+                    </RouterAnchor<AppRoute>>
+                </li>
+                <li class="nav-item">
+                    <RouterAnchor<AppRoute> route=AppRoute::Register classes="nav-link">
+                        { "Sign up" }
+                    </RouterAnchor<AppRoute>>
+                </li>
+            </ul>
+        }
+    }
 }
