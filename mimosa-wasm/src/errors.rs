5ea5f7066f6819192fc0c453d2f8d887fd514266
walker <warriorsfly@gmail.com>
2020-04-18 01:46:45 +0800

    add requests as basic request

--- mimosa-wasm/src/errors.rs
+++ mimosa-wasm/src/errors.rs
@@ -1,1 +1,33 @@
-pub enum Error {}
+use crate::models::ErrorInfo;
+use thiserror::Error as ThisError;
+
+#[derive(ThisError, Clone, Debug)]
+pub enum Error {
+    /// 401
+    #[error("Unauthorized")]
+    Unauthorized,
+
+    /// 403
+    #[error("Forbidden")]
+    Forbidden,
+
+    /// 404
+    #[error("Not Found")]
+    NotFound,
+
+    /// 422
+    #[error("Unprocessable Entity: {0:?}")]
+    UnprocessableEntity(ErrorInfo),
+
+    /// 500
+    #[error("Internal Server Error")]
+    InternalServerError,
+
+    /// serde deserialize error
+    #[error("Deserialize Error")]
+    DeserializeError,
+
+    /// request error
+    #[error("Http Request Error")]
+    RequestError,
+}
